4adcb0b666c121202b724dc5020b0d269d1392c6
style <sistema5000smart@gmail.com>
2018-02-04 18:05:10 -0500

    agregando gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp');
+ var  minifyCSS = require('gulp-minify-css');
+ var sass = require('gulp-sass');
+ 
+
+
+
+ 
+gulp.task('sass', function () {
+  return gulp.src('bower_components/boostrap/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('recursos/css/sass_css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('bower_components/boostrap/scss/*.scss', ['sass']);
+});
+
+gulp.task('css', function() {
+  return gulp.src('recursos/css/*.css')
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('recursos/css/min'))
+});
